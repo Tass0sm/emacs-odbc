ac06b538707ce30ea7b9e23a298c6c7fab6911ab
Tassos Manganaris <tassos.manganaris@gmail.com>
2023-12-25 17:44:04 -0500

    Add define_function and nil utils

--- utils.c
+++ utils.c
@@ -1,5 +1,9 @@
 #include "utils.h"
 
+emacs_value nil(emacs_env *env) {
+  return env->intern(env, "nil");
+}
+
 emacs_value intern(emacs_env *env, const char *name) {
   return env->intern(env, name);
 }
@@ -18,6 +22,20 @@ emacs_value funcall(emacs_env *env, const char* fn, ptrdiff_t nargs, ...) {
   free(args);
   return val;
 }
+
+void define_function(emacs_env *env, const char *name,
+                     ptrdiff_t min_arity, ptrdiff_t max_arity,
+                     emacs_value (*function) (emacs_env *env,
+                                              ptrdiff_t nargs,
+                                              emacs_value* args,
+                                              void *data) EMACS_NOEXCEPT,
+                     const char *documentation)
+{
+  emacs_value fn = env->make_function(env, min_arity, max_arity, function,
+                                      documentation, NULL);
+  funcall(env, "fset", 2, intern(env, name), fn);
+}
+
 
 void provide(emacs_env *env, const char *feature) {
   funcall(env, "provide", 1, intern(env, feature));
--- utils.h
+++ utils.h
@@ -2,6 +2,15 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+emacs_value nil(emacs_env *env);
 emacs_value intern(emacs_env *env, const char *name);
 emacs_value funcall(emacs_env *env, const char* fn, ptrdiff_t nargs, ...);
+
+void define_function(emacs_env *env, const char *name,
+                     ptrdiff_t min_arity, ptrdiff_t max_arity,
+                     emacs_value (*function) (emacs_env *env,
+                                              ptrdiff_t nargs,
+                                              emacs_value* args,
+                                              void *data) EMACS_NOEXCEPT,
+                     const char *documentation);
 void provide(emacs_env *env, const char *feature);
